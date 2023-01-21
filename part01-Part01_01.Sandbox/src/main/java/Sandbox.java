c3597f888d61092e9431e4949c20b93ac3b63aaa
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2023-01-21 02:35:00 +0100

    feat: update soloution
    Task #Part01_01.Sandbox

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -3,6 +3,6 @@ public class Sandbox {
 
     public static void main(String[] args) {
         // Write your program here
-        // Line comment
+        System.out.println("Ada Lovelace");
     }
 }
