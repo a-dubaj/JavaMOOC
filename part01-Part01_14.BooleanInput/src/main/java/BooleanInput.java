cc2d309fb1a41ea3fb4bedc78cc4f434d40aa5fd
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2023-02-06 17:07:32 +0100

    feat: add solution for next exercise

--- part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -7,6 +7,8 @@ public class BooleanInput {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
-
+        System.out.println("Write something: ");
+        boolean value = Boolean.valueOf(scanner.nextLine());
+        System.out.println("True or false?" + value);
     }
 }
