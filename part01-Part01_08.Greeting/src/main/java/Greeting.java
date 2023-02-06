3d0fa09cdadaf64f20c9f0535daf949229947c28
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2023-02-06 14:45:33 +0100

    feat: add next solution for exercise

--- part01-Part01_08.Greeting/src/main/java/Greeting.java
+++ part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -7,7 +7,8 @@ public class Greeting {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
-
+        System.out.println("What's your name?");
+        String variable = scanner.nextLine();
+        System.out.println("Hi " + variable);
     }
 }
