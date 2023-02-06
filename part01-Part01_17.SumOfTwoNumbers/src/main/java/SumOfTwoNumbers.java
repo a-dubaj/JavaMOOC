0bce05bf2a58291e0e8412fcf7f99a2c71807532
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2023-02-06 17:45:48 +0100

    feat: add solution for next exercise

--- part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
+++ part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
@@ -7,6 +7,11 @@ public class SumOfTwoNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number: ");
+        int second = Integer.valueOf(scanner.nextLine());
+        int sum = first + second;
+        System.out.println("The sum of the numbers is: " + sum);
     }
 }
