ef45f878012ca798abedd18ae20c4d75c0726436
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2023-02-07 00:24:24 +0100

    feat: add solution for exercise

--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -8,5 +8,20 @@ public class SimpleCalculator {
 
         // Write your program here
 
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+
+        int sum = first + second;
+        int difference = first - second;
+        int multiplication = first * second;
+        double division = 1.0 * first / second;
+
+        System.out.println(first + " + " + second + " = " + sum);
+        System.out.println(first + " - " + second + " = " + difference);
+        System.out.println(first + " * " + second + " = " + multiplication);
+        System.out.println(first + " / " + second + " = " + division);
     }
 }
