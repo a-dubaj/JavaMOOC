7d0123773f792cfbfd8d28710495c8f6d78dc87e
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2023-02-06 17:04:46 +0100

    feat: add solution for next exercise

--- part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
+++ part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
@@ -7,6 +7,8 @@ public class DoubleInput {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
-
+        System.out.println("Give a number: ");
+        double value = Double.valueOf(scanner.nextLine());
+        System.out.println("You give a number: " + value);
     }
 }
