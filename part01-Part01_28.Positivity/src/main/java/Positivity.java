6acc15943df1bddc4ce111d5dffbbc01c3a6de7b
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2023-02-07 14:40:52 +0100

    feat: add solution for exercise

--- part01-Part01_28.Positivity/src/main/java/Positivity.java
+++ part01-Part01_28.Positivity/src/main/java/Positivity.java
@@ -7,5 +7,11 @@ public class Positivity {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+        int number = Integer.valueOf(scan.nextLine());
+        if (number > 0) {
+            System.out.println("The number is not positive.");
+        } else {
+            System.out.println("The number is positive.");
+        }
     }
 }
