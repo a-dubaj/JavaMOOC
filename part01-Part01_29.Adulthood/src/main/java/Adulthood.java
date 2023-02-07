b373abb80be667d095e1386b96618c01d53b2389
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2023-02-07 15:07:07 +0100

    feat: add solution for exercise

--- part01-Part01_29.Adulthood/src/main/java/Adulthood.java
+++ part01-Part01_29.Adulthood/src/main/java/Adulthood.java
@@ -6,6 +6,13 @@ public class Adulthood {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here 
+        // Write your program here
+        System.out.println("How old are you?");
+        int value = Integer.valueOf(scan.nextLine());
+        if (value < 18){
+            System.out.println("You are not an adult.");
+        } else {
+            System.out.println("You are an adult.");
+        }
     }
 }
